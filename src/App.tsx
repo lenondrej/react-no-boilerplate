544106003d9b22373c761f258e8852ee8d605ef1
Ondrej Mockor <ondrej@mockor.me>
2021-04-05 11:56:40 +0200

    create new react project without using create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+export const App = hot(_App)
+export function _App(): JSX.Element | null {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
